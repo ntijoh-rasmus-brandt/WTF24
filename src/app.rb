fd465d61e024f2520891fbb9d27ef44a6a4e981b
Rasmus-Brandt-NTI-Johanneberg <119052518+ntijoh-rasmus-brandt@users.noreply.github.com>
2024-04-11 15:52:32 +0200

    fixat alla errors för när jag implementerade tags

--- src/app.rb
+++ src/app.rb
@@ -25,9 +25,9 @@ class App < Sinatra::Base
     end
 
     get '/products/tag/:tag' do |tag|
-        @products = db.execute('SELECT * FROM products')
         @tags = db.execute('SELECT * FROM tags')
-        @product_tags = db.execute('SELECT * FROM tags INNER JOIN product_tags ON tags.id = product_tags.tag_id INNER JOIN products ON product_tags.product_id = products.id WHERE tags.tag_name = ?', tag)
+        @product_ids = db.execute('SELECT product_id FROM tags INNER JOIN product_tags ON tags.id = product_tags.tag_id INNER JOIN products ON product_tags.product_id = products.id WHERE tags.tag_name = ?', tag)
+        @products = db.execute('SELECT * FROM products')
         erb :'products/index'
     end
 
