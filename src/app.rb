af5f21d19d44d7ead4dcd8a72dfcfbba4d42fd65
Rasmus-Brandt-NTI-Johanneberg <119052518+ntijoh-rasmus-brandt@users.noreply.github.com>
2024-05-16 15:29:21 +0200

    username på review

--- src/app.rb
+++ src/app.rb
@@ -46,7 +46,7 @@ class App < Sinatra::Base
     
     get '/products/:id' do |id|
         @product = db.execute('SELECT * FROM products WHERE id = ?', id).first
-        @reviews = db.execute('SELECT * FROM reviews INNER JOIN product_reviews ON reviews.id = product_reviews.review_id INNER JOIN products ON product_reviews.product_id = products.id WHERE products.id = ?', id)
+        @reviews = db.execute('SELECT * FROM reviews INNER JOIN product_reviews ON reviews.id = product_reviews.review_id INNER JOIN products ON product_reviews.product_id = products.id INNER JOIN user_reviews ON product_reviews.review_id = user_reviews.review_id INNER JOIN users ON user_reviews.user_id = users.id WHERE products.id = ?', id)
         @product_tags = db.execute('SELECT * FROM tags INNER JOIN product_tags ON tags.id = product_tags.tag_id INNER JOIN products ON products.id = product_tags.product_id WHERE products.id = ?', id)
         sum_ratings = 0
         amount = 0.0
@@ -83,16 +83,20 @@ class App < Sinatra::Base
     end
 
     post '/products/create' do
-        file = params[:file][:tempfile]
-        file_name = SecureRandom.alphanumeric(16)
-        file_path = "img/product/#{file_name}.jpg"
+        if @user_access != 2
+            redirect "/"
+        else
+            file = params[:file][:tempfile]
+            file_name = SecureRandom.alphanumeric(16)
+            file_path = "img/product/#{file_name}.jpg"
 
-        File.open("public/#{file_path}", 'wb') do |f|
-            f.write(file.read)
+            File.open("public/#{file_path}", 'wb') do |f|
+                f.write(file.read)
+            end
+
+            result = db.execute('INSERT INTO products (name, description, price, image_path) VALUES (?, ?, ?, ?) RETURNING *', params[:name], params[:description], params[:price], file_path).first
+            redirect "/products/#{result["id"]}"
         end
-
-        result = db.execute('INSERT INTO products (name, description, price, image_path) VALUES (?, ?, ?, ?) RETURNING *', params[:name], params[:description], params[:price], file_path).first
-        redirect "/products/#{result["id"]}"
     end
 
     post '/products/tags' do
@@ -101,62 +105,87 @@ class App < Sinatra::Base
     end
 
     post '/products/:id/delete' do |id|
-        product = db.execute('SELECT FROM products WHERE id = ?', id)
-        File.delete(product['image_path'])
-        db.execute('DELETE FROM products WHERE id = ?', id)
-        redirect "/products"
+        if @user_access != 2
+            redirect "/"
+        else
+            product = db.execute('SELECT FROM products WHERE id = ?', id)
+            File.delete(product['image_path'])
+            db.execute('DELETE FROM products WHERE id = ?', id)
+            redirect "/products"
+        end
     end
 
     post '/products/:id/update/delete_tag/:tag_id' do |product_id, tag_id|
-        db.execute('DELETE FROM product_tags WHERE product_id = ? AND tag_id = ?', product_id, tag_id)
-        redirect "/products/#{product_id}/edit"
+        if @user_access != 2
+            redirect "/"
+        else
+            db.execute('DELETE FROM product_tags WHERE product_id = ? AND tag_id = ?', product_id, tag_id)
+            redirect "/products/#{product_id}/edit"
+        end
     end
 
     post '/products/:id/update/add_tag' do |product_id|
-        tag_id = params[:tag_select]
-        exists = db.execute('SELECT * FROM product_tags WHERE product_id = ? AND tag_id = ?', product_id, tag_id)
-        if exists.empty?
-            db.execute('INSERT INTO product_tags (product_id, tag_id) VALUES (?, ?)', product_id, tag_id)
+        if @user_access != 2
+            redirect "/"
+        else
+            tag_id = params[:tag_select]
+            exists = db.execute('SELECT * FROM product_tags WHERE product_id = ? AND tag_id = ?', product_id, tag_id)
+            if exists.empty?
+                db.execute('INSERT INTO product_tags (product_id, tag_id) VALUES (?, ?)', product_id, tag_id)
+            end
+            redirect "/products/#{product_id}/edit"
         end
-        redirect "/products/#{product_id}/edit"
     end
 
     post '/products/:id/update' do |id|
-        if params[:file] != nil
-            product = db.execute('SELECT FROM products WHERE id = ?', id)
-            File.delete(product['image_path'])
-            
-            file_name = SecureRandom.alphanumeric(16)
-            file = params[:file][:tempfile]
-            file_path = "img/product/#{file_name}.jpg"
-
-            File.open("public/#{file_path}", 'wb') do |f|
-                f.write(file.read)
-            end
-
-            result = db.execute('UPDATE products SET name = ?, description = ?, price= ?, image_path = ? WHERE id = ? RETURNING *', params[:name], params[:description], params[:price], file_path, id).first
+        if @user_access != 2
+            redirect "/"
         else
-            result = db.execute('UPDATE products SET name = ?, description = ?, price= ? WHERE id = ? RETURNING *', params[:name], params[:description], params[:price], id).first
+            if params[:file] != nil
+                product = db.execute('SELECT FROM products WHERE id = ?', id)
+                File.delete(product['image_path'])
+                
+                file_name = SecureRandom.alphanumeric(16)
+                file = params[:file][:tempfile]
+                file_path = "img/product/#{file_name}.jpg"
+
+                File.open("public/#{file_path}", 'wb') do |f|
+                    f.write(file.read)
+                end
+
+                result = db.execute('UPDATE products SET name = ?, description = ?, price= ?, image_path = ? WHERE id = ? RETURNING *', params[:name], params[:description], params[:price], file_path, id).first
+            else
+                result = db.execute('UPDATE products SET name = ?, description = ?, price= ? WHERE id = ? RETURNING *', params[:name], params[:description], params[:price], id).first
+            end
+            redirect "/products/#{result['id']}"
         end
-        redirect "/products/#{result['id']}"
     end
 
     post '/review/:id/create' do |id|
         result = db.execute('INSERT INTO reviews (rating, review) VALUES (?, ?) RETURNING *', params[:rating], h(params[:review])).first
         db.execute('INSERT INTO product_reviews (product_id, review_id) VALUES (?, ?)', id, result['id'])
+        db.execute('INSERT INTO user_reviews (user_id, review_id) VALUES (?, ?)', @user_id, result['id'])
         redirect "/products/#{id}"
     end
 
     get '/reviews/:id/delete' do |id|
-        @review = db.execute('SELECT * FROM reviews WHERE id = ?', id).first
-        erb :'reviews/delete'
+        if @user_access != 2
+            redirect "/"
+        else
+            @review = db.execute('SELECT * FROM reviews WHERE id = ?', id).first
+            erb :'reviews/delete'
+        end
     end
 
     post '/reviews/:id/delete' do |id|
-        product_id = db.execute('SELECT * FROM product_reviews WHERE review_id = ?', id).first['product_id']
-        db.execute('DELETE FROM product_reviews WHERE review_id = ?', id)
-        db.execute('DELETE FROM reviews WHERE id  = ?', id)
-        redirect "/products/#{product_id}"
+        if @user_access != 2
+            redirect "/"
+        else
+            product_id = db.execute('SELECT * FROM product_reviews WHERE review_id = ?', id).first['product_id']
+            db.execute('DELETE FROM product_reviews WHERE review_id = ?', id)
+            db.execute('DELETE FROM reviews WHERE id  = ?', id)
+            redirect "/products/#{product_id}"
+        end
     end
 
     get '/users/register' do
